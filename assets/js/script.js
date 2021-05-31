4bb7e55827dff0fa90a5d2b15affdc32fd8840e5
Matías <matiasvergaralizama@gmail.com>
2021-05-30 22:05:07 -0400

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+  $("a").click(function(event){
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    var gato = this.hash;
+
+    $("html, body").animate({
+      scrollTop: $(gato).offset().top
+    }, 800, function(){
+      window.location.hash = gato;
+    });
+  }
+  });
+  $('[data-bs-toggle="popover"]').popover();
+
+
+$('[data-toogle="tooltip"]').tooltip()
+
+$("#title ").on("dblclick", function(event){
+  $(this).css({
+    "color" : "red",
+  });
+});
